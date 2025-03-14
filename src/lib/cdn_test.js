b13246f1444875d1c82818836577a6b3365a1df2
Abhishek Kumar <gwydionmaranio@gmail.com>
2025-03-14 21:22:41 +0530

    update

--- src/lib/cdn_test.js
+++ src/lib/cdn_test.js
@@ -1,6 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
   console.log("dom loaded");
-
-  const container = document.getElementById("__modal_component");
-  console.log("loaded", container);
+  try {
+    const container = document.getElementById("__modal_component");
+    console.log("loaded", container);
+  } catch (e) {
+    console.log("some error occured");
+  }
 });
