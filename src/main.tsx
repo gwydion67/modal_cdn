257d1d9cedba34f400dc5244d822f1606b01391d
Abhishek Kumar <gwydionmaranio@gmail.com>
2025-03-26 16:44:40 +0530

    trying iframe

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,33 @@
+import { useEffect } from "react";
 import App from "./App";
 import { createRoot } from "react-dom/client";
-import styleText from "./App.css?inline";
+import Frame from "react-frame-component";
 //
+const TailwindLoader = ({ children }: { children: React.ReactNode }) => {
+  let iframe = document.getElementById("__modal_iframe") as HTMLIFrameElement;
+  if (!iframe || !iframe.contentWindow) {
+    console.error("ERROR: there was some issue in loading component");
+    return <></>;
+  }
+  useEffect(() => {
+    function etw() {
+      console.log("loading tailwind");
+      let iframeDoc = iframe.contentDocument || iframe.contentWindow?.document!;
+      let tw = iframeDoc.createElement("script");
+      tw.setAttribute("src", "https://cdn.tailwindcss.com");
+      tw.onload = function () {
+        iframeDoc.body.innerHTML = iframeDoc.body.innerHTML; //re render
+      };
+      iframeDoc.head.appendChild(tw);
+    }
+
+    etw();
+  }, []);
+
+  return <>{children}</>;
+};
 (function () {
   const mountWidget = () => {
-    // Find the container element
-    const container = document.createElement("div");
-
-    // Exit if container is not found
-    // Create container for React
     const domElement = document.getElementById("__modal_component");
     if (!domElement) {
       console.error(
@@ -17,32 +36,15 @@ import styleText from "./App.css?inline";
       return;
     }
 
-    // Get API key from data attribute
     const apiKey = domElement.getAttribute("data-api-key");
 
-    // Append container to shadow root
-    const shadowRoot = container.attachShadow({ mode: "open" });
-    domElement.appendChild(container);
-
-    const defStyle = document.createElement("style");
-    defStyle.textContent = `
-      :host {
-        all: initial;
-        display: contents;
-      };
-      :host * {
-        all: initial;
-      }
-    `;
-    shadowRoot.appendChild(defStyle);
-
-    // Initialize React and render the App
-    const style = document.createElement("style");
-    style.textContent = styleText;
-    shadowRoot.appendChild(style);
-
-    const reactRoot = createRoot(shadowRoot);
-    reactRoot.render(<App apiKey={apiKey} />);
+    createRoot(domElement).render(
+      <Frame id="__modal_iframe">
+        <TailwindLoader>
+          <App apiKey={apiKey} />
+        </TailwindLoader>
+      </Frame>,
+    );
   };
 
   // Determine when to mount the widget based on document state
@@ -54,3 +56,30 @@ import styleText from "./App.css?inline";
     setTimeout(mountWidget, 1000);
   }
 })();
+
+// import { StrictMode } from "react";
+// import { createRoot } from "react-dom/client";
+// import "./index.css";
+// import App from "./App.tsx";
+
+// createRoot(document.getElementById("root")!).render(
+//   <StrictMode>
+//     <App apiKey="dummyKey" />
+//   </StrictMode>,
+// );
+//
+// <script>
+//   function etw() {
+//     var iframe = document.getElementById("__modal_iframe");
+//     var iframeDoc = iframe.contentDocument || iframe.contentWindow.document;
+//     var tw = iframeDoc.createElement("script");
+//     tw.setAttribute("src", "https://cdn.tailwindcss.com");
+//     tw.onload = function () {
+//       iframeDoc.body.innerHTML = iframeDoc.body.innerHTML; //re render
+//     };
+//     iframeDoc.head.appendChild(tw);
+//   }
+//   window.addEventListener("DOMContentLoaded", function () {
+//     iframe.contentWindow.addEventListener("DOMContentLoaded", etw);
+//   });
+// </script>
